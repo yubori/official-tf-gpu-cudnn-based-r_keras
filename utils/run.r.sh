02ac2b94d85911c93ca045893e1aa472bcb5516c
yubori <vori003@gmail.com>
2019-02-26 08:42:13 +0000

    Add Dockerfile

---
+++ utils/run.r.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CUDA_DEV=
+CUR_UID=
+CUR_GID=$(id -g)
+while getopts "d:" opt; do
+    case "$opt" in
+        d)
+            CUDA_DEV="NVIDIA_VISIBLE_DEVICES=$OPTARG"
+            #CUDA_DEV="CUDA_VISIBLE_DEVICES=$OPTARG"
+            ;;
+    esac
+done
+# オプション部分を切り捨てる。
+shift `expr $OPTIND - 1`
+
+CUR_DIR=$(pwd)
+
+sudo docker run -u $(id -u):$(id -g) --runtime=nvidia -e $CUDA_DEV -it --rm -v $CUR_DIR:$CUR_DIR tf-gpu-py3-r-keras:1.0 sh -c "cd $CUR_DIR && R --no-save --no-restore <$*"
